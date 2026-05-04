2b66b9f25507572c98fb3b28b48ba7bdb0fe43c6
Abidan Brito <43681148+abidanBrito@users.noreply.github.com>
2026-05-05 00:37:37 +0100

    feat: distict colours for compared indices & sorted state

--- src/visualizer.cpp
+++ src/visualizer.cpp
@@ -38,19 +38,26 @@ auto SortingVisualizer::draw() const -> void
     const auto full_width = static_cast<float>(window_width);
     const auto full_height = static_cast<float>(window_height);
     const float x_step { full_width / static_cast<float>(num_elements) };
+    const float bar_width { x_step - bar_gap };
 
     for (size_t i {}; i < num_elements; i++)
     {
         const float height { 0.9f * full_height * elements_[i] };
+        const bool is_compared { i == compared_.first || i == compared_.second };
+        const auto color = sorted_ ? GREEN : is_compared ? VIOLET : WHITE;
         DrawRectangle(static_cast<int>(x_step * static_cast<float>(i)),
-                      static_cast<int>(full_height - height), static_cast<int>(x_step),
-                      static_cast<int>(height), WHITE);
+                      static_cast<int>(full_height - height), static_cast<int>(bar_width),
+                      static_cast<int>(height), color);
     }
 }
 
 auto SortingVisualizer::sort_step() -> void
 {
-    if (marker_idx_ >= num_elements) return;
+    if (marker_idx_ >= num_elements)
+    {
+        sorted_ = true;
+        return;
+    }
 
     size_t min_idx { marker_idx_ };
     for (size_t i { marker_idx_ }; i < num_elements; i++)
@@ -62,5 +69,7 @@ auto SortingVisualizer::sort_step() -> void
     }
 
     std::swap(elements_[marker_idx_], elements_[min_idx]);
+    compared_ = { min_idx, marker_idx_ };
+
     marker_idx_++;
 }
--- src/visualizer.hpp
+++ src/visualizer.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <utility>
 
 class SortingVisualizer
 {
@@ -15,7 +16,10 @@ private:
     static constexpr int window_width { 800 };
     static constexpr int window_height { 600 };
     static constexpr int num_elements { 100 };
+    static constexpr float bar_gap { 0.1f };
 
     std::vector<float> elements_;
+    std::pair<size_t, size_t> compared_;
     size_t marker_idx_ { 0 };
+    bool sorted_ { false };
 };
